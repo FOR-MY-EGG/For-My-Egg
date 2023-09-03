13370ef7ec50c774d8813fff5c89ec28c86b7c32
jungeunevepark <evecomcom98@naver.com>
2023-09-03 14:04:03 +0900

    Init: backend 프로젝트 생성

---
+++ backend/src/main/java/com/shinhan/formyegg/FormyeggApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.formyegg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormyeggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormyeggApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/shinhan/formyegg/FormyeggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.formyegg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormyeggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
