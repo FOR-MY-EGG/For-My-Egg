1e03b110293fb967ffc536a0df0e765446be0385
김민태 <kimmintae981019@gmail.com>
2023-09-03 20:03:48 +0900

    Feat: ChatController Swagger API 정보 등록

--- backend/src/main/java/com/shinhan/formyegg/api/chat/controller/ChatController.java
+++ backend/src/main/java/com/shinhan/formyegg/api/chat/controller/ChatController.java
@@ -2,6 +2,8 @@ package com.shinhan.formyegg.api.chat.controller;
 
 import com.shinhan.formyegg.domain.chat.dto.ChatListRes;
 import com.shinhan.formyegg.domain.chat.service.ChatService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Api(value = "채팅 API", tags = {"Chat"})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/chat")
 public class ChatController {
     private final ChatService chatService;
 
+    @ApiOperation(value = "채팅 내역 조회", notes = "커뮤니티 타입으로 채팅 내역 조회")
     @GetMapping("/{affiliation}")
     public ResponseEntity<List<ChatListRes>> getChatRoomsByMemberNo(@PathVariable int affiliation) {
         return ResponseEntity.ok().body(chatService.findChatsByAffiliation(affiliation));
