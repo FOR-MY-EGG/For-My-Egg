2ec5b6dd45e2033fb514e26fa763311b327bd0d7
jungeunevepark <evecomcom98@naver.com>
2023-09-04 22:10:05 +0900

    Feat: ErrorMessage를 만들어주는 함수 생성

--- backend/src/main/java/com/shinhan/formyegg/global/error/ErrorResponse.java
+++ backend/src/main/java/com/shinhan/formyegg/global/error/ErrorResponse.java
@@ -1,5 +1,10 @@
 package com.shinhan.formyegg.global.error;
 
+import java.util.List;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,8 +16,27 @@ public class ErrorResponse {
 
 	public static ErrorResponse of(String errorCode, String errorMessage) {
 		return ErrorResponse.builder()
-			.errorCode(errorCode).errorMessage(errorMessage)
+			.errorCode(errorCode)
+			.errorMessage(errorMessage)
 			.build();
+	}
+
+	private static String createErrorMessage(BindingResult bindingResult) {
+		StringBuilder sb = new StringBuilder();
+		boolean isFirst = true;
+		List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+		for (FieldError fieldError : fieldErrors) {
+			if (!isFirst) {
+				sb.append(", ");
+			} else {
+				isFirst = false;
+			}
+			sb.append("[");
+			sb.append(fieldError.getField());
+			sb.append("]");
+			sb.append(fieldError.getDefaultMessage());
+		}
+		return sb.toString();
 	}
 
 }
