e56810de2824a4f55456f4f4c492b332edd68fe4
jungeunevepark <evecomcom98@naver.com>
2023-09-04 22:58:02 +0900

    Feat: BusinessException에 @Getter 어노테이션 추가

--- backend/src/main/java/com/shinhan/formyegg/global/error/exception/BusinessException.java
+++ backend/src/main/java/com/shinhan/formyegg/global/error/exception/BusinessException.java
@@ -2,10 +2,13 @@ package com.shinhan.formyegg.global.error.exception;
 
 import com.shinhan.formyegg.global.error.ErrorCode;
 
-public class BusinessException extends RuntimeException{
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
 	private ErrorCode errorCode;
 
-	public BusinessException(ErrorCode errorCode){
+	public BusinessException(ErrorCode errorCode) {
 		super(errorCode.getMessage());
 		this.errorCode = errorCode;
 	}
