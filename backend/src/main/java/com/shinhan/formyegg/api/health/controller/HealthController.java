fa07de5c870d21b4603b286100616f49ee2f0feb
evecomcom98 <evecomcom98@naver.com>
2023-09-04 21:25:08 +0900

    Feat: HealthController 생성 및 서버 상태를 체크하는 api 생성

---
+++ backend/src/main/java/com/shinhan/formyegg/api/health/controller/HealthController.java
@@ -0,0 +1,22 @@
+package com.shinhan.formyegg.api.health.controller;
+
+import com.shinhan.formyegg.api.health.dto.HealthResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+    private final Environment environment;
+
+    @GetMapping("/health")
+    public ResponseEntity<HealthResponseDto> healthCheck(){
+        HealthResponseDto healthResponseDto = HealthResponseDto.of("ok", Arrays.asList(environment.getActiveProfiles()));
+        return ResponseEntity.ok(healthResponseDto);
+    }
+}
