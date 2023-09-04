e69609c9b884b2963ac3905094e9faf77f71b36e
evecomcom98 <evecomcom98@naver.com>
2023-09-04 21:24:47 +0900

    Feat: HealthResponseDto 정적 팩토리 메서드 생성

--- backend/src/main/java/com/shinhan/formyegg/api/health/dto/HealthResponseDto.java
+++ backend/src/main/java/com/shinhan/formyegg/api/health/dto/HealthResponseDto.java
@@ -10,4 +10,11 @@ import java.util.List;
 public class HealthResponseDto {
     private String health;
     private List<String> activeProfiles;
+
+    public static HealthResponseDto of(String health, List<String> activeProfiles){
+        return  HealthResponseDto.builder()
+                .health(health)
+                .activeProfiles(activeProfiles)
+                .build();
+    }
 }
