87da8898ebc2c3b5e960fa0a382aafd5358f8d98
mi2ntae <kimmintae981019@gmail.com>
2023-09-04 22:07:00 +0900

    Fix: 주석 처리 제거

--- backend/src/main/java/com/shinhan/formyegg/domain/chat/service/ChatServiceImpl.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/chat/service/ChatServiceImpl.java
@@ -1,22 +1,22 @@
-// package com.shinhan.formyegg.domain.chat.service;
-//
-// import com.shinhan.formyegg.domain.chat.dto.ChatListRes;
-// import com.shinhan.formyegg.domain.chat.repository.ChatRepo;
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.slf4j.Slf4j;
-// import org.springframework.stereotype.Service;
-//
-// import java.util.List;
-//
-// @Service
-// @RequiredArgsConstructor
-// @Slf4j
-// public class ChatServiceImpl implements ChatService {
-//     private final ChatRepo chatRepo;
-//
-//     @Override
-//     public List<ChatListRes> findChatsByAffiliation(int affiliation) {
-//         return chatRepo.findChatByAffiliationAndOrderByCreateDate(affiliation);
-//     }
-// }
-//
+ package com.shinhan.formyegg.domain.chat.service;
+
+ import com.shinhan.formyegg.domain.chat.dto.ChatListRes;
+ import com.shinhan.formyegg.domain.chat.repository.ChatRepository;
+ import lombok.RequiredArgsConstructor;
+ import lombok.extern.slf4j.Slf4j;
+ import org.springframework.stereotype.Service;
+
+ import java.util.List;
+
+ @Service
+ @RequiredArgsConstructor
+ @Slf4j
+ public class ChatServiceImpl implements ChatService {
+     private final ChatRepository chatRepository;
+
+     @Override
+     public List<ChatListRes> findChatsByAffiliation(int affiliation) {
+         return chatRepository.findChatByAffiliationAndOrderByCreateDate(affiliation);
+     }
+ }
+
