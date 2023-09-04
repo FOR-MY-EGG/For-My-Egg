385b5cb3a8b411797ad275eda1ae25c801bf2eb8
jungeunevepark <evecomcom98@naver.com>
2023-09-04 23:10:33 +0900

    Fix: Member가 BaseTimeEntity를 상속하도록 수정

--- backend/src/main/java/com/shinhan/formyegg/domain/BaseTimeEntity.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/BaseTimeEntity.java
@@ -1,19 +1,21 @@
 package com.shinhan.formyegg.domain;
 
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-    @Column(name = "writtentime", columnDefinition = "DATETIME")
-    @CreatedDate
-    private LocalDateTime createDate;
+	@Column(columnDefinition = "DATETIME")
+	@CreatedDate
+	private LocalDateTime createDate;
 }
