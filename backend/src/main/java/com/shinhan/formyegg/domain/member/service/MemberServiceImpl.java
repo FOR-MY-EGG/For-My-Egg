2b6d8ddf998cc3c73f38c6d27c0746769024e968
evecomcom98 <evecomcom98@naver.com>
2023-09-05 20:44:52 +0900

    Feat: MemberServiceImpl에 MemberService를 상속

--- backend/src/main/java/com/shinhan/formyegg/domain/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/member/service/MemberServiceImpl.java
@@ -1,14 +1,41 @@
 package com.shinhan.formyegg.domain.member.service;
 
+import com.shinhan.formyegg.domain.member.dto.MemberDto;
 import org.springframework.stereotype.Service;
 
 import com.shinhan.formyegg.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
-public class MemberServiceImpl {
+public class MemberServiceImpl implements MemberService{
 	private final MemberRepository memberRepository;
 
+	@Override
+	public MemberDto createMember(MemberDto memberDto) {
+		return null;
+	}
+
+	@Override
+	public Optional<MemberDto> getMemberByKakaoId(String kakaoId) {
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<MemberDto> getMemberByMemberId(Long memberId) {
+		return Optional.empty();
+	}
+
+	@Override
+	public MemberDto updateNickname(MemberDto memberDto) {
+		return null;
+	}
+
+	@Override
+	public MemberDto updateProfile(MemberDto memberDto) {
+		return null;
+	}
 }
