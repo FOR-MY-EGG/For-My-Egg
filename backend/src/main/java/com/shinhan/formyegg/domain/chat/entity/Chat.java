f0e8e31a6536b04c13dcf39041ea597a4cbaa00b
evecomcom98 <evecomcom98@naver.com>
2023-09-05 20:27:42 +0900

    Fix: Chat Entity 수정

--- backend/src/main/java/com/shinhan/formyegg/domain/chat/entity/Chat.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/chat/entity/Chat.java
@@ -3,28 +3,33 @@ package com.shinhan.formyegg.domain.chat.entity;
 import com.shinhan.formyegg.domain.BaseTimeEntity;
 import com.shinhan.formyegg.domain.member.entity.Member;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
 @Entity
-@Data
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 public class Chat extends BaseTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column
     private long chatId;
 
-    @ManyToOne
-    @JoinColumn(name="memberId")
-    private Member member;
-    private String content;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member memberId;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
     private int affiliation;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
     private int type;
+
+    private String content;
 }
