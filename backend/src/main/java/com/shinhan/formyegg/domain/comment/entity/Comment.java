2d85efc8d13f25bac184686ad8547b7b68ba53bf
evecomcom98 <evecomcom98@naver.com>
2023-09-05 20:28:23 +0900

    Feat: Comment Entity 생성

---
+++ backend/src/main/java/com/shinhan/formyegg/domain/comment/entity/Comment.java
@@ -0,0 +1,31 @@
+package com.shinhan.formyegg.domain.comment.entity;
+
+import com.shinhan.formyegg.domain.BaseTimeEntity;
+import com.shinhan.formyegg.domain.board.entity.Board;
+import com.shinhan.formyegg.domain.member.entity.Member;
+import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long commentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="board_id")
+    private Board boardId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="member_id")
+    private Member memberId;
+
+    private String content;
+}
