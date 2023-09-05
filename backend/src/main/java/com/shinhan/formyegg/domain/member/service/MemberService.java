69df9894474be3df75ec240e487d91581f9b0d27
evecomcom98 <evecomcom98@naver.com>
2023-09-05 22:12:13 +0900

    Fix: 함수의 반환 타입 변경

--- backend/src/main/java/com/shinhan/formyegg/domain/member/service/MemberService.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/member/service/MemberService.java
@@ -7,9 +7,9 @@ import com.shinhan.formyegg.domain.member.dto.MemberDto;
 public interface MemberService {
 	MemberDto createMember(MemberDto memberDto);
 
-	Optional<MemberDto> getMemberByKakaoId(String kakaoId);
+	MemberDto getMemberByKakaoId(String kakaoId);
 
-	Optional<MemberDto> getMemberByMemberId(Long memberId);
+	MemberDto getMemberByMemberId(Long memberId);
 
 	MemberDto updateNickname(MemberDto memberDto);
 
