70559f4eb83449e5fbccba856a6c930e6e70b396
evecomcom98 <evecomcom98@naver.com>
2023-09-05 22:11:44 +0900

    Feat: 반환값을 저장하는 MemberResponse 생성

---
+++ backend/src/main/java/com/shinhan/formyegg/api/member/dto/MemberResponse.java
@@ -0,0 +1,33 @@
+package com.shinhan.formyegg.api.member.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.shinhan.formyegg.domain.member.dto.MemberDto;
+import lombok.*;
+
+@Builder
+@Getter
+@JsonNaming(value= PropertyNamingStrategies.SnakeCaseStrategy.class)
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberResponse {
+    private Long memberId;
+    private String address;
+    private int incomeRank;
+    private String name;
+    private String nickname;
+    private String kakaoId;
+    private String kakaoToken;
+
+    public static MemberResponse from(MemberDto memberDto){
+        return MemberResponse.builder()
+                .memberId(memberDto.getMemberId())
+                .address(memberDto.getAddress())
+                .incomeRank(memberDto.getIncomeRank())
+                .name(memberDto.getName())
+                .nickname(memberDto.getNickname())
+                .kakaoId(memberDto.getKakaoId())
+                .kakaoToken(memberDto.getKakaoToken())
+                .build();
+    }
+}
