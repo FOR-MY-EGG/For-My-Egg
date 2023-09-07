5a917d321c3554a79dd5cf53ff7b42fd70e1abfe
kosigi19 <kosigi19@naver.com>
2023-09-07 22:08:17 +0900

    Feat: 조회 조건 GroupId로 변경

--- backend/src/main/java/com/shinhan/formyegg/domain/child/repository/ChildRepository.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/child/repository/ChildRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
-    List<Child> findAllByAccountId(Long accountId);
+    List<Child> findAllByGroupId(Long groupId);
 }
