de175a361d964f42bcb2faada94c361820abb964
evecomcom98 <evecomcom98@naver.com>
2023-09-07 20:31:07 +0900

    Refactor: 필요없는 import 제거

--- backend/src/main/java/com/shinhan/formyegg/domain/group/repository/GroupRepository.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/group/repository/GroupRepository.java
@@ -3,7 +3,6 @@ package com.shinhan.formyegg.domain.group.repository;
 import com.shinhan.formyegg.domain.group.entity.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface GroupRepository extends JpaRepository<Group, Long> {
