417d13be4d9dba60577b95c2a769ae5cfd7deeb6
Dagon <leedagon624@gmail.com>
2023-09-07 20:59:47 +0900

    feat: FinanceRepository 수정

--- backend/src/main/java/com/shinhan/formyegg/domain/policy/repository/PolicyRepository.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/policy/repository/PolicyRepository.java
@@ -2,10 +2,14 @@ package com.shinhan.formyegg.domain.policy.repository;
 
 import com.shinhan.formyegg.domain.policy.entity.Policy;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.Optional;
 
-import java.util.List;
 
 public interface PolicyRepository extends JpaRepository <Policy, Long>{
+    @Query("SELECT p FROM Policy p WHERE p.targetStart <= :targetValue AND p.targetEnd >= :targetValue")
+    Optional<Policy> findPolicyInRange(@Param("targetValue") int targetValue);
 
-    public List<Policy> findAll();
+    Optional<Policy> findByChildCnt(int childCnt);
 }
