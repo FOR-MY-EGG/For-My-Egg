012d311121e754e4a619670f51885f3fe905f460
Dagon <leedagon624@gmail.com>
2023-09-07 21:10:22 +0900

    feat: FinanceRepository findall 추가

--- backend/src/main/java/com/shinhan/formyegg/domain/financeproduct/repository/FinanceRepository.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/financeproduct/repository/FinanceRepository.java
@@ -4,9 +4,12 @@ import com.shinhan.formyegg.domain.financeproduct.entity.FinanceProduct;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FinanceRepository extends JpaRepository<FinanceProduct, Long> {
+
+    List<FinanceProduct> findAll();
     Optional<FinanceProduct> findByType(String type);
 }
