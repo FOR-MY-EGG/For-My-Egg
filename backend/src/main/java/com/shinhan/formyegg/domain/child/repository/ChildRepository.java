c022a9b6ae55d6af48ed37e9a215de2d1ae97ee5
kosigi19 <kosigi19@naver.com>
2023-09-08 19:02:33 +0900

    Feat: findAllByChildId 추가

--- backend/src/main/java/com/shinhan/formyegg/domain/child/repository/ChildRepository.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/child/repository/ChildRepository.java
@@ -4,7 +4,9 @@ import com.shinhan.formyegg.domain.child.entity.Child;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ChildRepository extends JpaRepository<Child, Long> {
     List<Child> findAllByGroupId(Long groupId);
+    Optional<Child> findAllByChildId(Long childId);
 }
