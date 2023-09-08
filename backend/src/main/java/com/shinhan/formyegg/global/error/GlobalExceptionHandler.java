b2048737a45809320ce39aaca141f8d834796c97
mi2ntae <kimmintae981019@gmail.com>
2023-09-08 19:05:48 +0900

    Feat: JWT 토큰 인증 구현

--- backend/src/main/java/com/shinhan/formyegg/global/error/GlobalExceptionHandler.java
+++ backend/src/main/java/com/shinhan/formyegg/global/error/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.shinhan.formyegg.global.error;
 
+import com.shinhan.formyegg.global.error.exception.CAuthenticationEntryPointException;
+import com.shinhan.formyegg.global.error.exception.NonValidJwtTokenException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
@@ -15,6 +18,23 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+	@ExceptionHandler(CAuthenticationEntryPointException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected ResponseEntity<ErrorResponse> authenticationEntryPointException(Exception e) {
+		log.error("authentication exception");
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(errorResponse);
+	}
+
+	@ExceptionHandler(NonValidJwtTokenException.class)
+	protected ResponseEntity<ErrorResponse> nonValidJwtTokenException(BindException e) {
+		log.error("jwt error", e);
+		ErrorResponse errorResponse = ErrorResponse.of(HttpStatus.UNAUTHORIZED.toString(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+				.body(errorResponse);
+	}
 
 	@ExceptionHandler(BindException.class)
 	protected ResponseEntity<ErrorResponse> handleBindException(BindException e) {
