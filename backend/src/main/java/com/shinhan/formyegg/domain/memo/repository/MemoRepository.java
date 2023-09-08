d2c24e2fdab201c635076052800721d507c6e83b
evecomcom98 <evecomcom98@naver.com>
2023-09-08 18:54:05 +0900

    Feat: MemoRepository 생성 및 날짜정보로 메모 조회하는 함수 선언

---
+++ backend/src/main/java/com/shinhan/formyegg/domain/memo/repository/MemoRepository.java
@@ -0,0 +1,14 @@
+package com.shinhan.formyegg.domain.memo.repository;
+
+import com.shinhan.formyegg.domain.child.entity.Child;
+import com.shinhan.formyegg.domain.group.entity.Group;
+import com.shinhan.formyegg.domain.memo.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByGroupIdAndChildIdAndCreateDateBetween(Group groupId, Child childId, LocalDateTime startDate,
+                                                              LocalDateTime endDate);
+}
