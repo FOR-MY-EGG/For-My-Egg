a0bf1e6b95016566d878b48d2456368cdfdb4b42
evecomcom98 <evecomcom98@naver.com>
2023-09-08 20:24:48 +0900

    Feat: MemoDto에 정적 팩토리 메서드 생성

--- backend/src/main/java/com/shinhan/formyegg/domain/memo/dto/MemoDto.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/memo/dto/MemoDto.java
@@ -1,5 +1,7 @@
 package com.shinhan.formyegg.domain.memo.dto;
 
+import com.shinhan.formyegg.api.memo.dto.MemoReqDto;
+import com.shinhan.formyegg.domain.memo.entity.Memo;
 import lombok.Builder;
 import lombok.Getter;
 @Getter
@@ -11,5 +13,27 @@ public class MemoDto {
     private int amount;
     private String title;
     private String content;
+    private String holder;
     private String image;
+    public static MemoDto from(Memo memo){
+        return MemoDto.builder()
+                .memoId(memo.getMemoId())
+                .groupId(memo.getGroupId().getFamilyId())
+                .childId(memo.getChildId().getChildId())
+                .amount(memo.getAmount())
+                .title(memo.getTitle())
+                .content(memo.getContent())
+                .image(memo.getImage())
+                .holder(memo.getHolder())
+                .build();
+    }
+    public static MemoDto from(MemoReqDto memoReqDto){
+        return MemoDto.builder()
+                .childId(memoReqDto.getChildId())
+                .amount(memoReqDto.getAmount())
+                .title(memoReqDto.getTitle())
+                .content(memoReqDto.getContent())
+                .image(memoReqDto.getImage())
+                .build();
+    }
 }
