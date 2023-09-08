dc1a4edb51145884b25b94934de60500a77d85c7
evecomcom98 <evecomcom98@naver.com>
2023-09-08 21:51:09 +0900

    Fix: MemoResDto 변수 수정

--- backend/src/main/java/com/shinhan/formyegg/api/memo/dto/MemoResDto.java
+++ backend/src/main/java/com/shinhan/formyegg/api/memo/dto/MemoResDto.java
@@ -4,24 +4,18 @@ import com.shinhan.formyegg.domain.memo.dto.MemoDto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class MemoResDto {
-    private Long childId;
-    private int amount;
-    private String title;
-    private String content;
-    private String image;
-    private String holder;
+    private Long memoId;
+    private int day;
 
     public static MemoResDto from(MemoDto memoDto){
         return MemoResDto.builder()
-                .amount(memoDto.getAmount())
-                .childId(memoDto.getChildId())
-                .title(memoDto.getTitle())
-                .content(memoDto.getContent())
-                .image(memoDto.getImage())
-                .holder(memoDto.getHolder())
+                .memoId(memoDto.getMemoId())
+                .day(memoDto.getCreateDate().getDayOfMonth())
                 .build();
     }
 }
