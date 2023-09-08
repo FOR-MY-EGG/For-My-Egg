370a8cd04e1d51a7daa2ba39208338866cc992c1
evecomcom98 <evecomcom98@naver.com>
2023-09-08 20:26:25 +0900

    Feat: MemoService 생성 및 함수 구현

--- backend/src/main/java/com/shinhan/formyegg/domain/memo/service/MemoService.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/memo/service/MemoService.java
@@ -1,5 +1,7 @@
 package com.shinhan.formyegg.domain.memo.service;
 
+import com.shinhan.formyegg.api.memo.dto.MemoReqDto;
+import com.shinhan.formyegg.api.memo.dto.MemoResDto;
 import com.shinhan.formyegg.domain.memo.dto.MemoDto;
 
 import javax.swing.text.html.Option;
@@ -7,6 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemoService {
-    List<MemoDto> getMemoListByMonth(String month);
-    Optional<MemoDto> getMemoWhenToday();
+    MemoDto createMemo(Long groupId, MemoDto memoDto);
+    List<MemoDto> getMemoListByMonth(Long memberId, Long childId, int year, int month);
+    Optional<MemoDto> getMemoWhenToday(Long memberId, Long childId, int year, int month, int day);
 }
--- backend/src/main/java/com/shinhan/formyegg/domain/memo/service/MemoServiceImpl.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/memo/service/MemoServiceImpl.java
@@ -1,25 +1,49 @@
 package com.shinhan.formyegg.domain.memo.service;
 
+import com.shinhan.formyegg.domain.child.entity.Child;
+import com.shinhan.formyegg.domain.group.entity.Group;
+import com.shinhan.formyegg.domain.invitation.entity.Invitation;
+import com.shinhan.formyegg.domain.invitation.repository.InvitationRepository;
 import com.shinhan.formyegg.domain.memo.dto.MemoDto;
+import com.shinhan.formyegg.domain.memo.entity.Memo;
 import com.shinhan.formyegg.domain.memo.repository.MemoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class MemoServiceImpl implements MemoService{
-    private MemoRepository memoRepository;
+    private final MemoRepository memoRepository;
+    private final InvitationRepository invitationRepository;
 
-    @Override
-    public List<MemoDto> getMemoListByMonth(String month) {
-        return null;
+    public MemoDto createMemo(Long memberId, MemoDto memoDto){
+        Optional<Invitation> invitation = invitationRepository.findInvitationByMemberId(memberId);
+        Memo save = memoRepository.save(Memo.of(invitation.get().getFamilyId().getFamilyId(), memoDto));
+        return MemoDto.from(save);
     }
 
     @Override
-    public Optional<MemoDto> getMemoWhenToday() {
-        return Optional.empty();
+    public List<MemoDto> getMemoListByMonth(Long memberId, Long childId, int year, int month) {
+        Optional<Invitation> invitation = invitationRepository.findInvitationByMemberId(memberId);
+        LocalDateTime startDate = LocalDateTime.of(LocalDate.of(year, month, 1), LocalTime.of(0,0,0));
+        LocalDateTime endDate = LocalDateTime.of(LocalDate.of(year, month, 31), LocalTime.of(23,59,59));
+        List<Memo> memos = memoRepository.findAllByGroupIdAndChildIdAndCreateDateBetween(Group.from(invitation.get().getFamilyId().getFamilyId()), Child.from(childId), startDate, endDate);
+        return memos.stream().map(MemoDto::from).collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<MemoDto> getMemoWhenToday(Long memberId, Long childId, int year, int month, int day) {
+        Optional<Invitation> invitation = invitationRepository.findInvitationByMemberId(memberId);
+        LocalDateTime startDate = LocalDateTime.of(LocalDate.of(year, month, day), LocalTime.of(0,0,0));
+        LocalDateTime endDate = LocalDateTime.of(LocalDate.of(year, month, day), LocalTime.of(23,59,59));
+        List<Memo> memos = memoRepository.findAllByGroupIdAndChildIdAndCreateDateBetween(Group.from(invitation.get().getFamilyId().getFamilyId()), Child.from(childId), startDate, endDate);
+        return Optional.ofNullable(MemoDto.from(memos.get(0)));
     }
 }
