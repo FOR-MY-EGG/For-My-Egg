cb41ea08a143a65de8975b156931ac6c76463ec6
evecomcom98 <evecomcom98@naver.com>
2023-09-11 01:48:07 +0900

    Feat: 로그인 API 함수 구현

--- backend/src/main/java/com/shinhan/formyegg/api/member/controller/MemberController.java
+++ backend/src/main/java/com/shinhan/formyegg/api/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.shinhan.formyegg.api.member.dto.*;
 import com.shinhan.formyegg.domain.child.dto.ChildDto;
 import com.shinhan.formyegg.domain.member.dto.MemberDto;
 import com.shinhan.formyegg.domain.member.service.MemberService;
+import com.shinhan.formyegg.global.util.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,14 @@ import java.util.List;
 @RestController
 public class MemberController {
     private final MemberService memberService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping
+    public ResponseEntity<LoginResponse> kakaoLogin(@RequestBody MemberRequest member){
+        MemberDto memberDto = memberService.login(MemberDto.from(member));
+        String token = jwtTokenProvider.createToken(memberDto.getMemberId(), 1);
+        return ResponseEntity.status(HttpStatus.OK).body(LoginResponse.of(memberDto, token));
+    }
 
     @GetMapping("/{memberId}")
     public ResponseEntity<MemberResponse> getMember(@PathVariable Long memberId){
@@ -31,21 +40,10 @@ public class MemberController {
 
     }
 
-    @PostMapping
-    public ResponseEntity<MemberResponse> createMember(@RequestBody MemberRequest member){
-        MemberDto memberDto = memberService.createMember(MemberDto.from(member));
-        return ResponseEntity.status(HttpStatus.OK).body(MemberResponse.from(memberDto));
-    }
-
     @PatchMapping("/nickname")
     public ResponseEntity<MemberResponse> modifyNickname(@RequestBody MemberNicknameRequest member){
         MemberDto memberDto = memberService.updateNickname(MemberDto.from(member));
         return ResponseEntity.status(HttpStatus.OK).body(MemberResponse.from(memberDto));
     }
 
-    @PatchMapping("/profile")
-    public ResponseEntity<MemberResponse> modifyProfile(@RequestBody MemberProfileRequest member){
-        MemberDto memberDto = memberService.updateProfile(MemberDto.from(member));
-        return ResponseEntity.status(HttpStatus.OK).body(MemberResponse.from(memberDto));
-    }
 }
