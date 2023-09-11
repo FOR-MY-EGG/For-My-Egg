022412b0f2ae3282cad45b2d189db0f812cd906b
evecomcom98 <evecomcom98@naver.com>
2023-09-11 20:04:42 +0900

    Feat: 함수형 컴포넌트로 형식 변경 및 setMember 저장

--- frontend/formyegg/src/login/Login.js
+++ frontend/formyegg/src/login/Login.js
@@ -3,10 +3,60 @@ import { SafeAreaView, StyleSheet, Text, Image } from 'react-native';
 import  * as KakaoLogin from '@react-native-seoul/kakao-login';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import kakaoButton from 'formyegg/assets/images/kakao_login_symbol.png';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { setMember } from '../../reducers/memberReducer';
 
 
+export default function Login(){
+    const dispatch = useDispatch();
 
-const Login = () => {
+    const login = () => {
+        KakaoLogin.login().then((result) => {
+            console.log("Login Success", JSON.stringify(result));
+            const kakaoTokenJson = JSON.stringify(result);
+            const kakaoToken = JSON.parse(kakaoTokenJson);
+            getProfile(kakaoToken.accessToken);
+        }).catch((error) => {
+            if (error.code === 'E_CANCELLED_OPERATION') {
+                console.log("Login Cancel", error.message);
+        } else {
+            console.log(`Login Fail(code:${error.code})`, error.message);
+        }
+      });
+    };
+
+    const getProfile = (kakaoToken) => {
+        KakaoLogin.getProfile().then((result) => {
+          const kakaoUserJson = JSON.stringify(result);
+          const kakaoUser = JSON.parse(kakaoUserJson);
+          const data = {
+              kakao_token : kakaoToken,
+              kakao_id: kakaoUser.id,
+              nickname: kakaoUser.nickname
+          };
+          axios({
+              method: 'post',
+              url: 'http://10.0.2.2:8080/api/member',
+              data: data
+          }).then((response) => {
+                console.log(response.data.accessToken);
+              console.log(response.data.nickname);
+              console.log(response.data.memberId);
+              dispatch(setMember(response.data));
+              // console.log(JSON.stringify(response.data));
+              // console.log(response);
+          }).catch((error) =>{
+              console.log(error);
+          });
+      
+          console.log("GetProfile Success", JSON.stringify(result));
+        }).catch((error) => {
+          console.log(`GetProfile Fail(code:${error.code})`, error.message);
+        });
+    };
+
+
   return (
       <SafeAreaView>
         <TouchableOpacity
@@ -22,29 +72,6 @@ const Login = () => {
   );
 };
 
-const login = () => {
-    KakaoLogin.login().then((result) => {
-        console.log("Login Success", JSON.stringify(result));
-        getProfile();
-    }).catch((error) => {
-        if (error.code === 'E_CANCELLED_OPERATION') {
-            console.log("Login Cancel", error.message);
-    } else {
-        console.log(`Login Fail(code:${error.code})`, error.message);
-    }
-  });
-};
-
-const getProfile = () => {
-  KakaoLogin.getProfile().then((result) => {
-    const kakaoUserJson = JSON.stringify(result);
-    const kakaoUser = JSON.parse(kakaoUserJson);
-    console.log(kakaoUser.id);
-    console.log("GetProfile Success", JSON.stringify(result));
-  }).catch((error) => {
-    console.log(`GetProfile Fail(code:${error.code})`, error.message);
-  });
-};
 
 const styles = StyleSheet.create({
     kakaoButton : {
@@ -55,8 +82,9 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         margin: 10,
         padding: 20,
-        height: 80,
-        flexDirection :'row'
+        height: 100,
+        flexDirection :'row',
+        lineHeight: 100
     },
     kakaoSymbol : {
         height: 30,
@@ -64,4 +92,3 @@ const styles = StyleSheet.create({
         marginRight: 10
     }
 })
-export default Login;
