010675a309c30b10e98dc78314722e48d3561d9c
mi2ntae <kimmintae981019@gmail.com>
2023-09-12 22:17:40 +0900

    Feat: Chat 리스트 네이티브 쿼리 구현 및 DTO 수정

--- backend/src/main/java/com/shinhan/formyegg/domain/chat/entity/Chat.java
+++ backend/src/main/java/com/shinhan/formyegg/domain/chat/entity/Chat.java
@@ -1,5 +1,7 @@
 package com.shinhan.formyegg.domain.chat.entity;
 
+import com.shinhan.formyegg.api.board.dto.BoardDetailRes;
+import com.shinhan.formyegg.api.chat.dto.ChatRes;
 import com.shinhan.formyegg.domain.BaseTimeEntity;
 import com.shinhan.formyegg.domain.chat.dto.ChatDto;
 import com.shinhan.formyegg.domain.member.dto.MemberDto;
@@ -9,12 +11,37 @@ import lombok.*;
 import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
+@NamedNativeQueries(
+        @NamedNativeQuery(
+                name = "findChatsByAffiliation",
+                query = "SELECT c.chat_id as chatId, m.nickname as nickname, c.member_id as writer, c.type as type, c.content as content, c.create_date as createdDate " +
+                        "FROM Chat c JOIN Member m ON c.member_id = m.member_id " +
+                        "WHERE c.affiliation=:affiliation " +
+                        "ORDER BY createdDate",
+                resultSetMapping = "findChatsByAffiliation"
+        )
+)
+@SqlResultSetMapping(
+        name = "findChatsByAffiliation",
+        classes = @ConstructorResult(
+                targetClass = ChatRes.class,
+                columns = {
+                        @ColumnResult(name = "chatId", type = Long.class),
+                        @ColumnResult(name = "nickname", type = String.class),
+                        @ColumnResult(name = "writer", type = Long.class),
+                        @ColumnResult(name = "type", type = int.class),
+                        @ColumnResult(name = "content", type = String.class),
+                        @ColumnResult(name = "createdDate", type = LocalDateTime.class),
+                }
+        )
+)
 public class Chat extends BaseTimeEntity {
 
     @Id
