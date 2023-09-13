7734ad1f023496a10e5e0b20273880950b042809
mi2ntae <kimmintae981019@gmail.com>
2023-09-13 22:51:07 +0900

    Fix: Board 오류 해결

--- frontend/formyegg/app/screens/Board/BoardType3Screen.js
+++ frontend/formyegg/app/screens/Board/BoardType3Screen.js
@@ -27,7 +27,7 @@ const BoardType3Screen = ({navigation}) => {
         image: "https://user-images.githubusercontent.com/3969643/51441420-b41f1c80-1d14-11e9-9f5d-af5cd3a6aaae.png", 
         title: "yuyu", 
         view: 0,
-        createAt: "20202020", 
+        createAt: "2020-03-20 13:00", 
         content: "이번에는 리액트 네이티브(React Native)로 인스타그램 UI을 구현하는 포스팅입니다. 다른 앱을 따라 만들어 보는 것은 굉장히 재미있습니다. 구글에서 인스타그램 클론 코딩 강의를 찾아보니, 다른 개발자들이 올린 동영상 강의를 몇 개 찾을 수 있었습니다."}]);
     }, [])
   );
