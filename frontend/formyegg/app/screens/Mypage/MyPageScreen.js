5a9e1355b9ee63b66533e7a56671e2ed9e18031b
kosigi19 <kosigi19@naver.com>
2023-09-13 21:26:07 +0900

    Feat: 메인페이지 헤더 버튼 추가

--- frontend/formyegg/app/screens/Mypage/MyPageScreen.js
+++ frontend/formyegg/app/screens/Mypage/MyPageScreen.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import {Text, View} from 'react-native';
 
-const MyPageScreen = ({navigation}) => {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>MyPageScreen! 🎉</Text>
-      <Button title="정책" onPress={() => navigation.navigate('Chat')} />
-      <Button title="금융" onPress={() => navigation.navigate('Chat')} />
-    </View>
-  );
+const MyPageScreen = () => {
+  return <div>MyPageScreen</div>;
 };
 
 export default MyPageScreen;
