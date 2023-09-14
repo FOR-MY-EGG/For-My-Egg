5cd9c14350e9b50524aee2bf65432ebef7b78095
mi2ntae <kimmintae981019@gmail.com>
2023-09-14 20:20:26 +0900

    Feat: 메인화면 아이 불러오기

--- frontend/formyegg/app/screens/Home/HomeScreen.js
+++ frontend/formyegg/app/screens/Home/HomeScreen.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import {Text, View, Button} from 'react-native';
 import {setMember} from '../../../reducers/memberReducer';
-import {useDispatch} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import {Appbar} from 'react-native-paper';
+import http from "../../utils/commonHttp"
+import { useFocusEffect } from '@react-navigation/native';
+import { setChild } from '../../../reducers/childReducer';
 
 const HomeScreen = ({navigation}) => {
   const dispatch = useDispatch();
+  const memberId = useSelector((state) => state.member.memberId);
+  const [childs, setChilds] = useState([]);
+  // const child = useSelector((state) => state.child);
+  useFocusEffect(
+    useCallback(() => {
+      http.get(`member/all/${memberId}`)
+      .then((res) => {
+        let children = res.data.children;
+        if(children.length > 0) {
+          setChilds(children);
+          dispatch(setChild(children[0]))
+        }
+      })
+      .catch((err) => {
 
+      })
+    }, [])
+  )
   return (
     <>
       {/* <Appbar.Header>
