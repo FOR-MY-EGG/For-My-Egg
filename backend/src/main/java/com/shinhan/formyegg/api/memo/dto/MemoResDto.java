adc7acd6d81085b04250fe72d880e70c28e44114
evecomcom98 <evecomcom98@naver.com>
2023-09-15 00:39:38 +0900

    Fix: 메모 리스트의 반환형을 년-월-일 형식으로 변환

--- backend/src/main/java/com/shinhan/formyegg/api/memo/dto/MemoResDto.java
+++ backend/src/main/java/com/shinhan/formyegg/api/memo/dto/MemoResDto.java
@@ -4,18 +4,19 @@ import com.shinhan.formyegg.domain.memo.dto.MemoDto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Builder
 public class MemoResDto {
     private Long memoId;
-    private int day;
+    private LocalDate day;
 
     public static MemoResDto from(MemoDto memoDto){
         return MemoResDto.builder()
                 .memoId(memoDto.getMemoId())
-                .day(memoDto.getCreateDate().getDayOfMonth())
+                .day(memoDto.getCreateDate())
                 .build();
     }
 }
