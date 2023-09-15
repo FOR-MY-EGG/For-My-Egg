8bbc6c1fd496fef384a9e28b9c9716523de376c2
evecomcom98 <evecomcom98@naver.com>
2023-09-15 21:35:08 +0900

    Fix: accountReducer 추가

--- frontend/formyegg/reducer.js
+++ frontend/formyegg/reducer.js
@@ -3,17 +3,19 @@ import storage from "@react-native-async-storage/async-storage"
 import persistReducer from "redux-persist/es/persistReducer";
 import memberReducer from "./reducers/memberReducer";
 import childReducer from "./reducers/childReducer";
+import accountReducer from "./reducers/accountReducer";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["member", "child"],
+  whitelist: ["member", "child", "account"],
 };
 
 const rootReducer = combineReducers({
   // 필요한거 더 추가하세요
   member: memberReducer,
-  child: childReducer
+  child: childReducer,
+  account: accountReducer
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
