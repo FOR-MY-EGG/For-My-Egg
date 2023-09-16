efa3f57c7e871f54bef7654ee96aefe5b2452ccb
kosigi19 <kosigi19@naver.com>
2023-09-16 15:28:12 +0900

    Feat: 그룹 코드 클릭 시, 복사되도록 설정

--- frontend/formyegg/app/screens/Mypage/FamilyScreen.js
+++ frontend/formyegg/app/screens/Mypage/FamilyScreen.js
@@ -1,21 +1,30 @@
-import React, { useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, Button, StyleSheet, TouchableOpacity} from 'react-native';
 import Clip from 'react-native-vector-icons/Entypo';
 import http from '../../utils/commonHttp';
+import Clipboard from '@react-native-clipboard/clipboard';
 
 const FamilyScreen = ({navigation}) => {
   const [code, setCode] = useState('');
+  const onCopyAddress = async code => {
+    try {
+      await Clipboard.setString(code);
+    } catch (e) {
+      console.log('실패');
+    }
+  };
 
   useEffect(() => {
-    http.get("group/group-code")
-    .then((res) => {
-      console.log(res.data);
-      setCode(res.data);
-    })
-    .catch((err)=>{
-      console.log(err);
-    })
-}, []);
+    http
+      .get('group/group-code')
+      .then(res => {
+        console.log(res.data);
+        setCode(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
   return (
     <View
       style={{
@@ -24,34 +33,37 @@ const FamilyScreen = ({navigation}) => {
         alignItems: 'center',
       }}>
       <Text style={{fontSize: 20}}>아래에 초대코드를 공유하여</Text>
-      <Text style={{fontSize: 20, marginBottom: 10}}> 가족을 초대할 수 있습니다</Text>
+      <Text style={{fontSize: 20, marginBottom: 10}}>
+        {' '}
+        가족을 초대할 수 있습니다
+      </Text>
       <TouchableOpacity
-          style={{ 
-            width: '85%',
-            height: 60,
-            borderRadius: 12,
-            color: 'black',
-            backgroundColor: 'white',
-            justifyContent : 'center',
-            alignItems: 'center',
-            flexDirection : 'row'
-          }}   
-      >      
-          <Clip
-            name="clipboard"
-            size={17}
-          />
-        <Text style={styles.upload}>code</Text>
+        style={{
+          width: '85%',
+          height: 60,
+          borderRadius: 12,
+          color: 'black',
+          backgroundColor: 'white',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'row',
+        }}
+        onPress={() => {
+          onCopyAddress(code);
+        }}>
+        <Clip name="clipboard" size={17} />
+        <Text style={styles.upload}>{code}</Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  upload : {
+  upload: {
     color: 'black',
     textAlign: 'center',
     marginLeft: 5,
+    fontSize: 13,
     // width: "100%"
   },
 });
