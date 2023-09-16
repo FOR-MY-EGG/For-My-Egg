01b251a6937c28f98ad89fe5dbffcce11f919437
kosigi19 <kosigi19@naver.com>
2023-09-16 17:24:08 +0900

    Style: 로그인, 그룹코드 페이지 스타일 수정

--- frontend/formyegg/app/screens/Login/JoinScreen.js
+++ frontend/formyegg/app/screens/Login/JoinScreen.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import {Text, View, Button, StyleSheet, TextInput} from 'react-native';
-import { useDispatch, useSelector } from 'react-redux';
+import React, {useState} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import axios from 'axios';
-import { setGroupId } from '../../../reducers/memberReducer';
+import {setGroupId} from '../../../reducers/memberReducer';
+import {Button, TextInput} from 'react-native-paper';
 
 const JoinScreen = ({navigation}) => {
   const dispatch = useDispatch();
@@ -11,24 +12,25 @@ const JoinScreen = ({navigation}) => {
   const [text, setText] = useState('');
   const [inputText, setInputText] = useState('');
 
-
   const submitBtn = () => {
     setText(inputText);
     axios({
       method: 'post',
       url: 'http://10.0.2.2:8080/api/group/invitation',
-      data : {
-        uuid : inputText
+      data: {
+        uuid: inputText,
       },
       headers: {
-        authorization: `Bearer `+token
-      }
-    }).then((response) => {
+        authorization: `Bearer ` + token,
+      },
+    })
+      .then(response => {
         console.log(response.data.groupId);
         dispatch(setGroupId(response.data.groupId));
-    }).catch((error) =>{
+      })
+      .catch(error => {
         console.log(error);
-    });
+      });
   };
 
   return (
@@ -36,27 +38,46 @@ const JoinScreen = ({navigation}) => {
       style={{
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center',
+        marginHorizontal: 50,
       }}>
-      <Text>그룹 코드 내놔.🎉</Text>
-        <TextInput
-          style={styles.textInput}
-          onChangeText={(text) => setInputText(text)}
-          placeholder="그룹 코드를 입력해주세요."
-        />
-      <Button title="입력" onPress={() => submitBtn()} />
+      <Text style={{fontSize: 15, fontWeight: 'bold'}}>
+        그룹 코드를 입력해주세요
+      </Text>
+      {/* <TextInput
+        style={styles.textInput}
+        onChangeText={text => setInputText(text)}
+        placeholder="그룹 코드를 입력해주세요."
+      /> */}
+      <TextInput
+        // label="그룹 코드"
+        value={text}
+        // placeholder="그룹 코드를 입력해주세요."
+        mode="outlined"
+        outlineColor="transparent"
+        selectionColor="#A3C9B8"
+        activeOutlineColor="#A3C9B8"
+        onChangeText={text => setText(text)}
+        style={{marginBottom: 10, borderRadius: 50}}
+      />
+      <Button
+        title="Login"
+        mode="contained"
+        buttonColor="#A3C9B8"
+        textColor="#F6F6F6"
+        style={{borderRadius: 50}}
+        onPress={() => submitBtn()}>
+        등록
+      </Button>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textInput: {
-    width: 200,
-    height: 40,
-    borderWidth: 1,
-    borderColor: 'gray',
+    // borderWidth: 1,
+    // borderColor: 'gray',
     marginBottom: 10,
     padding: 8,
-  }
-})
+  },
+});
 export default JoinScreen;
