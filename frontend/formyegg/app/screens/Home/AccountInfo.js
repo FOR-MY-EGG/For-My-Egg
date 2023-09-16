794d5f5c32bbf2bb0e296f69bddb24c96cd3e73d
mi2ntae <kimmintae981019@gmail.com>
2023-09-16 12:42:14 +0900

    Feat: AccountInfo 화면 추가

---
+++ frontend/formyegg/app/screens/Home/AccountInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+    Text,
+  } from 'react-native';
+
+const AccountInfo = ({navigation}) => {
+  return (
+      <Text>asdas</Text>
+  );
+};
+
+export default AccountInfo;
